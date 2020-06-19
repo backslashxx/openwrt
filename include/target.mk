9bcf861080bcaef4c67b20362b1abf3450902873
Rui Salvaterra <rsalvaterra@gmail.com>
2020-06-19 10:45:25 +0100

    ARM Cortex-A9: build the userspace with Thumb-2 instructions
    Thumb-2 code is denser than pure ARM, reducing RAM usage and improving
    performance due to better instruction cache footprint.
    
    There's no reason for not enabling this feature on other ARMv7 targets
    (cortex-a7 and cortex-a8), but I don't have the hardware to test it.
    
    Signed-off-by: Rui Salvaterra <rsalvaterra at gmail.com>
    Tested-by: Tad <tad@spotco.us>

--- include/target.mk
+++ include/target.mk
@@ -252,6 +252,15 @@ ifeq ($(DUMP),1)
     CPU_CFLAGS_e5500:=-mcpu=e5500
     CPU_CFLAGS_powerpc64:=-mcpu=powerpc64
   endif
+  ifeq ($(ARCH),sparc)
+    CPU_TYPE = sparc
+    CPU_CFLAGS_ultrasparc = -mcpu=ultrasparc
+  endif
+  ifeq ($(ARCH),arm)
+    CPU_CFLAGS_cortex-a7 = -mthumb
+    CPU_CFLAGS_cortex-a8 = -mthumb
+    CPU_CFLAGS_cortex-a9 = -mthumb
+  endif
   ifeq ($(ARCH),aarch64)
     CPU_TYPE ?= generic
     CPU_CFLAGS_generic = -mcpu=generic
