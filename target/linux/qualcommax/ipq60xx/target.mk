d6f98c6cfc47e6a99cc25e54ea50609f2eb41e79
1Conan <me@1conan.com>
2024-04-05 22:21:42 +0800

    qualcommax: ipq60xx: performance optimizations
    Set the target arch to Cortex-A73. Packages are still set as A53 to use the their feeds. Locally built packages will use the A73 toolchain and optimizations.
    
    Co-authored-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- target/linux/qualcommax/ipq60xx/target.mk
+++ target/linux/qualcommax/ipq60xx/target.mk
@@ -1,5 +1,7 @@
 SUBTARGET:=ipq60xx
 BOARDNAME:=Qualcomm Atheros IPQ60xx
+CPU_TYPE:=cortex-a73
+ARCH_PACKAGES:=aarch64_cortex-a53
 DEFAULT_PACKAGES += ath11k-firmware-ipq6018
 
 define Target/Description
